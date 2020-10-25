962bedd1d66ad2cc75336d7262c587df47bc4b94
rochl <romchik2k@gmail.com>
2020-10-25 17:22:07 -0400

    First Commit

--- mvc/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$config = [
-    'MODEL_PATH' => APPLICATION_PATH . DS . 'model' . DS,
-    'VIEW_PATH' => APPLICATION_PATH . DS . 'view' . DS,
-    'LIB_PATH' => APPLICATION_PATH . DS . 'lib' . DS
-];
-
-require $config['LIB_PATH'] . 'functions.php';
